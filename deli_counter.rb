bc1d5a6ad0fdb398cd74948c795171593645fe67
Jennifer Dirmeyer <dirmeyer@gmail.com>
2019-05-10 02:33:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,10 @@
 def line(customer_list)
-  line_list = ""
+  line_list = "The line is currently:"
   if customer_list.length == 0 
     puts "The line is currently empty."
   else
     customer_list.each.with_index do |customer, i|
-      line_list << "#{i + 1}. #{customer}"
+      line_list << " #{i + 1}. #{customer}"
     end  
   end
 end
