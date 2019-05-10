95a24e60cdfd425e760d8bae0b4312036d66a0f5
Jennifer Dirmeyer <dirmeyer@gmail.com>
2019-05-10 02:27:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,5 @@
 def line(customer_list)
-  if customer_list.length == 0 
+  if customer_list.size == 0 
     "The line is currently empty."
   else
     customer_list.each.with_index do |customer, i|
