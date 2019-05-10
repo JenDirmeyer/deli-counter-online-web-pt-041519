1922f2ae5a7f6b49027719f8f733f0fb02b51028
Jennifer Dirmeyer <dirmeyer@gmail.com>
2019-05-10 02:45:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,3 +14,8 @@ def take_a_number(array,new_person)
   array << new_person
   puts "Welcome, #{new_person}. You are number #{array.index(new_person)+ 1} in line."
 end
+
+def now_serving(array)
+  if array.length == 0 
+    puts "The line is currently empty."
+  else  
