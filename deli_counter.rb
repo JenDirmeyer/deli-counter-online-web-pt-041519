c064dd76b71d7b54080711da7d1ff382664abb04
Jennifer Dirmeyer <dirmeyer@gmail.com>
2019-05-10 02:48:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,7 +19,7 @@ def now_serving(array)
   if array.length == 0 
     puts "There is nobody waiting to be served!"
   else
-    puts array.pop
+    puts "Currently serving #{array.shift}."
     array
   end   
 end  
